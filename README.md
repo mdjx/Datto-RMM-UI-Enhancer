1f73b317a10c4e148f7573e686cbd58b8e31322d
Milosh Djuric <md@syslab.com.au>
2019-11-27 18:17:49 +1030

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Datto RMM UI Enhancer
+
+This Chrome extension provides small enhancements to the Datto RMM UI. 
+
+## Disclaimer
+
+This extension is not affiliated or associated with Datto, Inc. in any way. It is an unofficial, third party browser extension made by a dRMM user. Use at your own risk.
+
+## Features
+
+- The Sites down menu does not truncate names and is longer, it also links to the Devices tab to each site instead of the Summary tab. 
+
+The Devices tab includes the bulk of the improvements
+- Device Hostname column: Windows Servers have a light blue background
+- Last Seen column: Replaces timestamp with age (example: 18 hours 27 minutes)
+- Last Audit column: Background color based on audit age (Green to Red gradient) and replaces the timestamp with age (for example: 5 hours 22 minutes)
+- Operating System Column: Replaces Windows 10 Build number (10.0.18362) with the friendlier and shorter version (1903)
+- Last Reboot column: Replaces timestamp with age (for example: 15 days 17 hours 8 minutes)
+- Reboot Required column: Red background if a reboot is pending
+
+![Extenstion Screenshot](docs/screenshot.png)
+
+## Installation
+
+### Via the Chrome Store
+
+The extension can be installed from the [Chrome Web Store](https://chrome.google.com/webstore/detail/datto-rmm-ui-enhancer/hipgdbnphembdgicadgomdoibpnppakd)
+
+### Manually
+
+Clone the repository and drag the `src` folder into the Chrome Extensions page. Developer mode will need to be enabled. 
---
+++ src/drmm.js
@@ -0,0 +1,215 @@
+window.onload = function() {
+  console.log("Datto RMM UI Enhancer Loaded");
+
+  fixDeviceTables();
+
+  // sites dropdown
+  document.getElementById("toptab_profiles").addEventListener(
+    "mouseover",
+    function() {
+      setTimeout(function() {
+        fixSitesDropdown();
+      }, 700);
+    },
+    { once: true }
+  );
+};
+
+// expands the sites dropdown and removes truncating from site names
+// sets default link when clicking on site to go do devices page
+function fixSitesDropdown() {
+  try {
+    document.getElementById("menuProfilesDiv").style["maxWidth"] = "550px";
+    document.querySelector("#menuProfilesDiv > div > ul").style["maxHeight"] =
+      "500px";
+    allSites = document.querySelectorAll(
+      "#menuProfilesDiv > div > ul > li > a"
+    );
+    allSites.forEach(site => {
+      site.innerText = site.title;
+      site.href = site.href.replace("summary", "listDevices");
+    });
+  } catch {}
+}
+
+function fixDeviceTables() {
+  // Device list changes
+  // Use regex to check for multiple urls that contain device lists (listDevies and filter)
+  if (/listDevices|filter/i.test(location.href)) {
+    const allRows = document.querySelectorAll("#listTable tr");
+
+    var tableHeadings = Array.from(document.getElementById("headTr").children);
+    var tableHeadings = tableHeadings.map(heading => heading.innerText.trim());
+
+    // Highlight required devices
+
+    allRows.forEach(row => {
+      try {
+        if (
+          row.children[
+            tableHeadings.indexOf("Reboot required")
+          ].innerText.includes("Yes")
+        ) {
+          row.children[
+            tableHeadings.indexOf("Reboot required")
+          ].style.background = "rgba(252, 207, 207, 0.4)";
+        }
+      } catch {}
+    });
+
+    // Highlights server hostnames on devices pages
+
+    allRows.forEach(row => {
+      try {
+        if (
+          row.children[
+            tableHeadings.indexOf("Operating System")
+          ].innerText.includes("Server")
+        ) {
+          row.children[
+            tableHeadings.indexOf("Device Hostname")
+          ].style.background = "rgba(95, 185, 185, 0.10)";
+        }
+      } catch {}
+    });
+
+    // Colour code audit age
+    currentDateTime = Date.now().toString();
+    oneHourinMS = 3600000;
+    to = moment();
+
+    allRows.forEach(row => {
+      try {
+        // get last audit timestamp, strip timezone
+        lastAudit = Date.parse(
+          row.children[tableHeadings.indexOf("Last Audit Date")].innerText
+            .replace(/[A-Z]/g, "")
+            .trim()
+        );
+
+        hoursDifference = Number.parseFloat(
+          (currentDateTime - lastAudit) / (1000 * 60 * 60)
+        ).toPrecision(3);
+
+        H = (255 - Math.pow(hoursDifference, 2)) * 0.4; // Hue (0.4 = Green)
+        H = H < 1 ? 1 : H; // ensures we do not have a negative value
+        S = 0.9; // Saturation
+        B = 0.9; // Brightness
+
+        if (hoursDifference > 0) {
+          hourDesc = hoursDifference == 1 ? "hour" : "hours";
+          from = moment(lastAudit);
+          timeSinceLastAudit = moment.preciseDiff(from, to); // '1 month 2 days 3 hours 4 minutes 5 seconds'
+
+          row.children[
+            tableHeadings.indexOf("Last Audit Date")
+          ].style.background = `hsla(${H},90%,90%,0.4)`;
+          row.children[
+            tableHeadings.indexOf("Last Audit Date")
+          ].innerText = timeSinceLastAudit;
+        }
+      } catch {}
+    });
+
+    // Change Last Reboot to Uptime value (days, hours, minutes) instead of timestamp
+    currentDateTime = Date.now().toString();
+
+    allRows.forEach((row, index) => {
+      try {
+        lastReboot = Date.parse(
+          row.children[tableHeadings.indexOf("Last Reboot")].innerText
+            .replace(/[A-Z]/g, "")
+            .trim()
+        );
+
+        secondsDifference = Number.parseFloat(
+          (currentDateTime - lastReboot) / 1000
+        ).toPrecision(2);
+        if (secondsDifference > 0) {
+          var seconds = secondsDifference; //parseInt(secondsDifference,10);
+
+          var days = Math.floor(seconds / (3600 * 24));
+          seconds -= days * 3600 * 24;
+          var hrs = Math.floor(seconds / 3600);
+          seconds -= hrs * 3600;
+          var mnts = Math.floor(seconds / 60);
+          seconds -= mnts * 60;
+
+          dayDesc = days == 1 ? "day" : "days";
+          hourDesc = hrs == 1 ? "hour" : "hours";
+          minDesc = mnts == 1 ? "minute" : "minutes";
+
+          from = moment(lastReboot);
+          timeSinceLastReboot = moment.preciseDiff(from, to); // '1 month 2 days 3 hours 4 minutes 5 seconds'
+          row.children[
+            tableHeadings.indexOf("Last Reboot")
+          ].innerText = `${timeSinceLastReboot}`;
+        }
+      } catch {
+        console.log(`Last reboot failure on index ${index} `);
+      }
+    });
+
+    // Change Last Seen to Uptime value (days, hours, minutes) instead of timestamp
+    allRows.forEach((row, index) => {
+      try {
+        // Check if device is currently online, and if so, skips the rest
+        if (
+          row.children[tableHeadings.indexOf("Last Seen")].innerText.trim() !=
+          "Currently Online"
+        ) {
+          lastSeen = Date.parse(
+            row.children[tableHeadings.indexOf("Last Seen")].innerText
+              .replace(/[A-Z]/g, "")
+              .trim()
+          );
+
+          secondsDifference = Number.parseFloat(
+            (currentDateTime - lastSeen) / 1000
+          ).toPrecision(2);
+          if (secondsDifference > 0) {
+            var seconds = secondsDifference; //parseInt(secondsDifference,10);
+
+            var days = Math.floor(seconds / (3600 * 24));
+            seconds -= days * 3600 * 24;
+            var hrs = Math.floor(seconds / 3600);
+            seconds -= hrs * 3600;
+            var mnts = Math.floor(seconds / 60);
+            seconds -= mnts * 60;
+
+            dayDesc = days == 1 ? "day" : "days";
+            hourDesc = hrs == 1 ? "hour" : "hours";
+            minDesc = mnts == 1 ? "minute" : "minutes";
+
+            from = moment(lastSeen);
+            timeSinceLastSeen = moment.preciseDiff(from, to); // '1 month 2 days 3 hours 4 minutes 5 seconds'
+            //console.log(`Duration: ${currentDateTime}, ${lastSeen}`)
+            row.children[
+              tableHeadings.indexOf("Last Seen")
+            ].innerText = `${timeSinceLastSeen}`;
+          }
+        }
+      } catch {
+        console.log(`Last seen failure on index ${index} `);
+      }
+    });
+
+    // Clean up OS Column
+    allRows.forEach(row => {
+      os = row.children[
+        tableHeadings.indexOf("Operating System")
+      ].innerText.trim();
+      os = os.replace("Microsoft", "");
+
+      // Windows 10 versions
+      os = os.replace("10.0.16299", "1709");
+      os = os.replace("10.0.17134", "1803");
+      os = os.replace("10.0.17763", "1809");
+      os = os.replace("10.0.18362", "1903");
+      os = os.replace("10.0.18363", "1909");
+
+      // Set OS
+      row.children[tableHeadings.indexOf("Operating System")].innerText = os;
+    });
+  }
+}
